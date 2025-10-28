166a5c184ca2e48a0b284f6fcaacec4d4ae545da
Brian Futrell <bfutrel@gmail.com>
2025-10-28 11:40:02 -0400

    module 3: mocking to isolate code

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,8 +1,10 @@
+import { of } from "rxjs";
 import { HeroesComponent } from "./heroes.component"
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES;
+  let mockHeroService;
 
   beforeEach(() => {
     HEROES = [
@@ -12,5 +14,27 @@ describe('HeroesComponent', () => {
     ]
   })
 
-  component = new HeroesComponent({});
+  // creates a mock instance of an object
+  mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+  component = new HeroesComponent(mockHeroService);
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the list', () => {
+      // have to tell the mock service to return an observable
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      // this expect only verifies that a hero has been removed,
+      // but not specifically the one that was specified.
+      // --- instructor's expect
+      //expect(component.heroes.length).toBe(2);
+
+      // this expect filters for any hero that has the ID
+      // of the hero that was deleted, should be 0
+      expect(component.heroes.filter((h) => h.id === 3).length).toBe(0);
+    });
+  })
 })
