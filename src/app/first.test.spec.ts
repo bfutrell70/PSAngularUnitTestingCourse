27d56b16765a20f5920255ad64108f9ea548f9b7
Brian Futrell <bfutrel@gmail.com>
2025-10-28 09:08:11 -0400

    craeted sample test, commented out

---
+++ src/app/first.test.spec.ts
@@ -0,0 +1,20 @@
+// ---- sample test ----
+// describe("my first test", () => {
+//   let sut;
+
+//   // run before each test (it represents a test)
+//   beforeEach(() => {
+//     sut = {};
+//   });
+
+//   it('should be true if true', () => {
+//     // arrange
+//     sut.a = false;
+
+//     // act
+//     sut.a = true;
+
+//     //assert
+//     expect(sut.a).toBe(true);
+//   });
+// });
