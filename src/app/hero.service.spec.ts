63bda29239d0bdf9fcc2c530610dee87b0620aa5
Brian Futrell <bfutrel@gmail.com>
2025-10-29 09:13:08 -0400

    module 5: implemeting a test with mocked HTTP

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing"
+import { inject, TestBed } from "@angular/core/testing"
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
 // isn't automatically added when adding HttpClientTestingModule for the first time
@@ -8,6 +8,7 @@ import { HttpClientTestingModule, HttpTestingController} from "@angular/common/h
 describe('HeroService', () => {
     let mockMessageService;
     let httpTestingController: HttpTestingController;
+    let service: HeroService;
 
     beforeEach(() => {
         mockMessageService = jasmine.createSpyObj(['add']);
@@ -24,8 +25,28 @@ describe('HeroService', () => {
 
         // finds the service that corellates to the type passed into it
         httpTestingController = TestBed.inject(HttpTestingController);
-        let msgSvc = TestBed.inject(MessageService);
+        
+        // get a handle to a service
+        service = TestBed.inject(HeroService);
     })
 
+    describe("getHero", () => {
+        // order of parameter is important
+        // works but is more complex
+        // it('should call get with the correct URL', inject([HeroService, HttpTestingController], 
+        //     (service: HeroService, controller: HttpTestingController) => {
+        //     // call getHero()
+        //     service.getHero(4);
 
+        //     // test that the URL was correct
+        // }))
+
+        it('should call het with the correct URL', () => {
+            // call getHero();
+            service.getHero(4);
+
+            // test that the URL is correct
+            httpTestingController.
+        })
+    })
 })
